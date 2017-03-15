a3f721a74cfe3d51ee216f8147d5694042018928
tbae2 <thomas.bae0@gmail.com>
2017-03-15 11:04:46 -0400

    flexbox, break out search function to its own, css tweaks, removed results div

--- nearby.js
+++ nearby.js
@@ -10,7 +10,6 @@ let currentPosition = {
 //       currentPosition.longitude = position.coords.longitude;
 // });
 
-
 document.addEventListener('DOMContentLoaded', function() {
 
     var maps_file = document.createElement('script');
@@ -19,58 +18,61 @@ document.addEventListener('DOMContentLoaded', function() {
     maps_file.src = "https://maps.googleapis.com/maps/api/js?libraries=places&callback=initMap&key=AIzaSyAYzBUsoyit6PKN0XCpQLg0RcYSs9seE_A";
     document.getElementsByTagName('head')[0].appendChild(maps_file);
 
-
 });
 
 //map variable that can be used for map call backs
 let map;
-
+let service;
+let places = [];
 //initialize map object and assign to map variable
-function initMap(){
-      //console.log(currentPosition);
-      map = new google.maps.Map(document.querySelector('.map'),{
-        center: {lat: 24.0686755,lng: -34.4854967},
-          zoom: 15
-      });
-      //if geolocation is available use it
-      if(navigator.geolocation){
+function initMap() {
+    //console.log(currentPosition);
+    map = new google.maps.Map(document.querySelector('.map'), {
+        center: {
+            lat: 24.0686755,
+            lng: -34.4854967
+        },
+        zoom: 15
+    });
+    //if geolocation is available use it
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             currentPosition.lat = position.coords.latitude;
             currentPosition.lng = position.coords.longitude;
-            // let pos = {
-            //   lat: position.coords.latitude,
-            //   lng: position.coords.longitude
-            // };
-
-          new google.maps.Marker({position: currentPosition,map:map});
-          map.setCenter(currentPosition);
-          console.log(currentPosition);
-
+            new google.maps.Marker({position: currentPosition, map: map});
+            map.setCenter(currentPosition);
         });
-      }
+      };
 
-      const searchQuery = document.querySelector('.search-locations');
-      let searchBox = new google.maps.places.SearchBox(searchQuery);
-      map.controls[google.maps.ControlPosition.TOP_CENTER].push(searchQuery);
+    service = new google.maps.places.PlacesService(map);
 
-// Bias the SearchBox results towards current map's viewport.
-map.addListener('bounds_changed', function() {
-  searchBox.setBounds(map.getBounds());
-});
+    const searchQuery = document.querySelector('.search-locations');
+    let searchBox = new google.maps.places.SearchBox(searchQuery);
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(searchQuery);
 
-searchBox.addListener('places_changed', function(){
-    let places = searchBox.getPlaces();
-    let resultList = document.querySelector('.locations');
-    console.log(resultList);
-    resultList.innerHTML = places.map(location => {
-                                    return `<li>${location.name}</li>`;
-                                }).join('');
-                                console.log(resultList);
-})
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
 
+    searchBox.addListener('places_changed', function() {
+        places = searchBox.getPlaces();
+        search(places);
+      });
+};
 
-      // let home = new google.maps.Marker({
-      //   position: {lat: 34.0686755,lng: -84.4854967},
-      //   map: map
-      // });
+function search(places){
+
+  let resultList = document.querySelector('.locations');
+  console.log(places);
+  resultList.innerHTML = places.map(location => {
+      return `<div class="resultitem">
+                                        <div class="resultcontent"><img src=${location.icon} /></div>
+                                        <div class="resultcontent"><p>${location.name}</p></div>
+                                        <div class="resultcontent"><p>${location.formatted_address}</p></div>
+                                      </div>`;
+  }).join('');
+
+  console.log(resultList);
+
 };
