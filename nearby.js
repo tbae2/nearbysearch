17a331d68bb7792e52911d23ef178d2215b795b7
tbae2 <thomas.bae0@gmail.com>
2017-03-22 15:55:44 -0400

    adding marker drop functionality

--- nearby.js
+++ nearby.js
@@ -33,7 +33,7 @@ function initMap() {
             lat: 24.0686755,
             lng: -34.4854967
         },
-        zoom: 15
+        zoom: 13
     });
     //if geolocation is available use it
     if (navigator.geolocation) {
@@ -43,7 +43,7 @@ function initMap() {
             new google.maps.Marker({position: currentPosition, map: map});
             map.setCenter(currentPosition);
         });
-      };
+    };
 
     //attach the Places service to this map
     service = new google.maps.places.PlacesService(map);
@@ -59,65 +59,80 @@ function initMap() {
     //listener to figure out what the user selected.
     searchBox.addListener('places_changed', function() {
 
-      let sectionedPlaces = [];
+        let sectionedPlaces = [];
         places = searchBox.getPlaces();
         //console.log(places.length);
         // search(places);
-          if(places.length === 0){
+        if (places.length === 0) {
             console.log('places empty');
             return;
-          }
-        for(var i = 0; i < places.length; i += 4){
-            sectionedPlaces.push(places.slice(i,i + 4));
+        }
+        for (var i = 0; i < places.length; i += 4) {
+            sectionedPlaces.push(places.slice(i, i + 4));
         }
 
-
-          //takes an array of promises, maps over them and returns only when they're all complete
-        sectionedPlaces.forEach(function(placeSection){
-          //work around the 5 queries a second limit imposed by Google
-            setTimeout(function(){
-                  let placePromises = placeSection.map(function(place){
-                        return searchPlaces(place);
-                  });
-
-                  Promise.all(placePromises)
-                      .then(function(result){
-                          renderList(result);
-                      })
+        sectionedPlaces.forEach(function(placeSection) {
+            //work around the 5 queries a second limit imposed by Google
+            setTimeout(function() {
+                // build an array of promises
+                let placePromises = placeSection.map(function(place) {
+                    return searchPlaces(place);
+                });
+                //takes an array of promises, maps over them and returns only when they're all complete
+                Promise.all(placePromises).then(function(result) {
+                    renderList(result);
+                })
             }, sectionedPlaces.indexOf(placeSection) * 1400);
         });
 
-      });
+    });
 };
 
-function searchPlaces(placeQuery){
-  //return a promise object
-  return new Promise(
-        function(resolve, reject){
-          service.getDetails({placeId: placeQuery.place_id}, (place, status) =>
-              {
-                if(status === google.maps.places.PlacesServiceStatus.OK){
-                      resolve(place);
-                  } else {
-                      reject(status);
-                  }
-            });
-          });
+function searchPlaces(placeQuery) {
+    //return a promise object
+    return new Promise(function(resolve, reject) {
+        service.getDetails({
+            placeId: placeQuery.place_id
+        }, (place, status) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                resolve(place);
+            } else {
+                reject(status);
+            }
+        });
+    });
 };
 
-function renderList(locations){
+function renderList(locations) {
     console.log(locations);
-        let addResult = document.createElement('div');
-        let resultLocations = document.querySelector('.locations');
+    let addResult = document.createElement('div');
+    let resultLocations = document.querySelector('.locations');
 
-       addResult.innerHTML = locations.map(location => {
-       return `<div class="resultitem">
+    addResult.innerHTML = locations.map(location => {
+        addMarkers(location);
+        return `<div class="resultitem">
                <div class="resultcontent"><img src=${location.icon} /></div>
                <div class="resultcontent"><p>${location.name}</p></div>
                <div class="resultcontent"><p>${location.formatted_address}</p></div>
                </div>`;
-                          }).join('');
+    }).join('');
 
-        resultLocations.appendChild(addResult);
+    resultLocations.appendChild(addResult);
+};
+
+function addMarkers(place) {
+    let marker = new google.maps.Marker({
+        title: place.name,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        position: place.geometry.location,
+        icon: {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        }
+    });
 
 };
